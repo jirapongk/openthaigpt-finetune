19a07871bbdae11787d3879574eb8af595d983a5
t0led0 <turboman_t@hotmail.com>
2023-11-18 23:20:03 +0700

    change device to cpu

--- merge_lora_model.py
+++ merge_lora_model.py
@@ -8,12 +8,13 @@ if len(sys.argv) != 4:
     print("Usage: python merge_lora_model.py <base_model> <lora_dir> <output_dir>")
     exit()
 
-device_map = {"": 0}
+# device_map = {"": 0}
+device_map = {"": "cpu"}
 lora_dir = sys.argv[2]
 # base_model_name = "meta-llama/Llama-2-7b-hf"
 base_model_name = sys.argv[1]
 tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
-model = AutoPeftModelForCausalLM.from_pretrained(lora_dir, device_map={"": "cpu"}, torch_dtype=torch.bfloat16)
+model = AutoPeftModelForCausalLM.from_pretrained(lora_dir, device_map=device_map, torch_dtype=torch.bfloat16)
 
 
 model = model.merge_and_unload()
